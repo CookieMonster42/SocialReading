1114e004d0f644335a4365d9d430b8ef628bc38f
CM <a.richert2111@icloud.com>
2019-11-26 14:36:53 +0100

    populated homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @groups = Group.all
+    @memberships = Membership.all
+    @comments = Comment.all
+    @books = Book.all
+    @languages = Language.all
+    @users = User.all
   end
 end
