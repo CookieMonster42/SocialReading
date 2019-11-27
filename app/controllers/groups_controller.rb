cb81695aef1aa5b9af693aaf19d32398c1bcd405
CM <a.richert2111@icloud.com>
2019-11-27 14:46:19 +0100

    got the pundit working

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,12 @@
 class GroupsController < ApplicationController
-
   def show
     @group = Group.find(params[:id])
     @comment = Comment.new
+    skip_authorization
   end
 
   def index
     @groups = Group.all
+    skip_policy_scope
   end
 end
