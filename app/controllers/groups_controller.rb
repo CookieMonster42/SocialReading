162551db9e2e7613ac863d3e3471695bd9872a62
Stella <55156346+stellatmn@users.noreply.github.com>
2019-11-27 14:51:45 +0100

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,7 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
     @comment = Comment.new
+    # for now ok but later make no external see the group
     skip_authorization
   end
 
