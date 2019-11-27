52355beb4eeecec19309cead9ae7fc103ccb6a0b
CM <a.richert2111@icloud.com>
2019-11-27 16:43:55 +0100

    did the search logic

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,5 +9,21 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
     skip_policy_scope
+    @location = params[:format]
+    search_groups(@location)
+  end
+
+  private
+
+  def search_groups(location)
+    if params[:query].present?
+      if params[:range].values.first.to_i.zero?
+        range = 50
+      else
+        range = params[:range].values.first.to_i
+      end
+      @groups = Group.near(" #{params[:query]}", 50)
+      redirect_to groups_path
+    end
   end
 end
