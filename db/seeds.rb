0af0887ce710f376f7f845f99c0ef31defafe4e4
CM <a.richert2111@icloud.com>
2019-11-28 12:08:05 +0100

    added profilepictures and chajnged the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,53 +21,63 @@ puts "Languages seeded!"
 ###########
 # User/host seed
 ###########
+
+names = ["Andy", "Lala", "Kevin", "Jay", "May", "Des", "Roy"]
+
 host = User.create({
   name: "Andy",
   password: '123456',
   email: 'andy@smile.com',
-  bio: 'I love fiction and Babelsberg, I also have some dogs'
+  bio: 'I love fiction and Babelsberg, I also have some dogs',
+  photo: "shining-cover.jpg"
 })
 
 host1 = User.create({
   name: "Lala",
   password: '123456',
   email: 'lala@smile.com',
-  bio: 'I love fiction and Babelsberg, I also have some dogs'
+  bio: 'I love fiction and Babelsberg, I also have some dogs',
+  photo: "shining-cover.jpg"
 })
 
 host2 = User.create({
   name: "Kevin",
   password: '123456',
   email: 'kevin@smile.com',
-  bio: 'I love fiction and Babelsberg, I also have some dogs'
+  bio: 'I love fiction and Babelsberg, I also have some dogs',
+  photo: "shining-cover.jpg"
 })
 
 host3 = User.create({
   name: "Jay",
   password: '123456',
   email: 'jay@smile.com',
-  bio: 'I love fiction and Babelsberg, I also have some dogs'
+  bio: 'I love fiction and Babelsberg, I also have some dogs',
+  photo: "shining-cover.jpg"
 })
 
 host4 = User.create({
   name: "May",
   password: '123456',
   email: 'may@smile.com',
-  bio: 'I love fiction and Babelsberg, I also have some dogs'
+  bio: 'I love fiction and Babelsberg, I also have some dogs',
+  photo: "shining-cover.jpg"
 })
 
 host5 = User.create({
   name: "Des",
   password: '123456',
   email: 'des@smile.com',
-  bio: 'I love fiction and Babelsberg, I also have some dogs'
+  bio: 'I love fiction and Babelsberg, I also have some dogs',
+  photo: "shining-cover.jpg"
 })
 
 host6 = User.create({
   name: "Roy",
   password: '123456',
   email: 'roy@smile.com',
-  bio: 'I love fiction and Babelsberg, I also have some dogs'
+  bio: 'I love fiction and Babelsberg, I also have some dogs',
+  photo: "shining-cover.jpg"
 })
 
 puts "Users seeded!"
@@ -136,7 +146,7 @@ puts "Books seeded!"
 ###########
 # group seed
 ###########
-group = Group.new({
+group1 = Group.new({
   name: "Clever Witches",
   location: "Kottbusser Damm 104 Berlin",
   host_message: 'This book is starring at me for years now. I always wanted to read it. It’s time to improve my German skills. Don’t ask me why German, but hey, fuck it. Let’s rock. ',
@@ -147,7 +157,7 @@ group.book = book
 group.language = language
 group.save
 
-group = Group.new({
+group2 = Group.new({
   name: "Find The Real You",
   location: "Adalbertstraße 70 Berlin",
   host_message: 'This book is starring at me for years now. I always wanted to read it. It’s time to improve my German skills. Don’t ask me why German, but hey, fuck it. Let’s rock. ',
@@ -158,7 +168,7 @@ group.book = book1
 group.language = language
 group.save
 
-group = Group.new({
+group3 = Group.new({
   name: "Be Real Adventurer",
   location: "Reichenberger Str. 36 Berlin",
   host_message: 'This book is starring at me for years now. I always wanted to read it. It’s time to improve my German skills. Don’t ask me why German, but hey, fuck it. Let’s rock. ',
@@ -169,7 +179,7 @@ group.book = book2
 group.language = language
 group.save
 
-group = Group.new({
+group4 = Group.new({
   name: "Who is U",
   location: "Cuccuma, Zossener Str. 34 Berlin",
   host_message: 'This book is starring at me for years now. I always wanted to read it. It’s time to improve my German skills. Don’t ask me why German, but hey, fuck it. Let’s rock. ',
@@ -180,7 +190,7 @@ group.book = book3
 group.language = language
 group.save
 
-group = Group.new({
+group5 = Group.new({
   name: "Out of My Mind",
   location: "Lima",
   host_message: 'This book is starring at me for years now. I always wanted to read it. It’s time to improve my German skills. Don’t ask me why German, but hey, fuck it. Let’s rock. ',
@@ -191,7 +201,7 @@ group.book = book4
 group.language = language
 group.save
 
-group = Group.new({
+group6 = Group.new({
   name: "We are The Children",
   location: "Two and Two, Pannierstraße 6, Berlin",
   host_message: 'This book is starring at me for years now. I always wanted to read it. It’s time to improve my German skills. Don’t ask me why German, but hey, fuck it. Let’s rock. ',
@@ -202,7 +212,7 @@ group.book = book5
 group.language = language
 group.save
 
-group = Group.new({
+group7 = Group.new({
   name: "Better Way of Thinking",
   location: "Moscow",
   host_message: 'This book is starring at me for years now. I always wanted to read it. It’s time to improve my German skills. Don’t ask me why German, but hey, fuck it. Let’s rock. ',
@@ -226,6 +236,15 @@ membership_user1 = Membership.new()
 membership_user1.group = group
 membership_user1.user = host1
 membership_user1.save
+
+
+Membership.create({user: host1, group: group1})
+Membership.create({user: host2, group: group2})
+Membership.create({user: host3, group: group3})
+Membership.create({user: host4, group: group4})
+Membership.create({user: host5, group: group5})
+Membership.create({user: host6, group: group6})
+Membership.create({user: host7, group: group7})
 
 puts "Memberships seeded!"
 ###########
