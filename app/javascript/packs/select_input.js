86b842ad74bcf529e00d8fcc48096d4b02a4a4b0
CookieMonster42 <47447669+CookieMonster42@users.noreply.github.com>
2019-12-02 20:47:22 +0100

    Update select_input.js

--- app/javascript/packs/select_input.js
+++ app/javascript/packs/select_input.js
@@ -1,12 +1,1 @@
-const fomth = () => {
-  const range = document.querySelector("#range");
-  if (range) {
-    console.log(range)
-    range.addEventListener("focus", (event) => {
-      console.log(event);
-      console.log(event.currentTarget);
-    });
-  }
-};
 
-export { fomth }
