a027398446c2408f439d5facba9d5904cfc8297b
CookieMonster42 <47447669+CookieMonster42@users.noreply.github.com>
2019-12-03 10:14:00 +0100

    Revert "Index/search section"

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,7 +13,6 @@ class GroupsController < ApplicationController
   end
 
   def index
-    @languages = Language.all
     skip_policy_scope
     search_groups(params)
   end
@@ -21,19 +20,20 @@ class GroupsController < ApplicationController
   private
 
   def search_groups(params)
-    @tags_all = ActsAsTaggableOn::Tag.all.map { |instance| instance.name }
-    @location = params[:query]
-    @range = params[:range]
-    @range = 50 if @range.empty?
-    @language = params[:search][:language]
-    @language = Language.first.id if @language.empty?
-    @tags_given = params[:search][:tags].empty? ? @tags_all : params[:search][:tags]
-
-    if !@location.empty?
-      # if more tags than one are given the any should be all in line 28
-      @groups = Group.near(@location, @range).where(language_id: @language).tagged_with(@tags_given, any: true)
-    elsif @location.empty?
-      @groups = Group.where(language_id: @language).tagged_with(@tags_given, any: true)
+    searched = false
+    if params[:query]
+      @location = params[:query]
+      searched = true
+      @groups = Group.near(@location, 50)
+    elsif params[:format]
+      @location = params[:format]
+      searched = true
+      @groups = Group.near(@location, 50)
+    else
+      @groups = Group.all
+    end
+    if @groups.any? && searched
+      @groups = Group.near(@location, 50)
     else
       @groups = Group.all
     end
