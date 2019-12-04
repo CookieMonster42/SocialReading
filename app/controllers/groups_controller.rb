e8d67097b593654ab22ad55a333268d126f69471
Anja Laube <mail@anjalaube.com>
2019-12-04 12:58:42 +0100

    implements tag field on group form

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -29,6 +29,7 @@ class GroupsController < ApplicationController
     @group.user_id = current_user.id
     @group.book = Book.find_by(title: params[:book][:title])
     @group.language = Language.find_by(name: params[:language][:name])
+    @group.tag_list.add(params[:group][:tags])
     if @group.save!
       redirect_to group_path(@group)
     else
@@ -37,6 +38,7 @@ class GroupsController < ApplicationController
   end
 
   def index
+    @languages = Language.all
     @tags_all = ActsAsTaggableOn::Tag.all.map { |instance| instance.name }
     skip_policy_scope
     search_groups(params)
@@ -49,7 +51,6 @@ class GroupsController < ApplicationController
   end
 
   def search_groups(params)
-    @languages_all = Language.all.ids
     @tags_all = ActsAsTaggableOn::Tag.all.map { |instance| instance.name }
     @location = params[:query]
     if @range.nil?
@@ -59,17 +60,8 @@ class GroupsController < ApplicationController
       @range = params[:range].empty? ? 50 : params[:range]
     end
     # @range = 50 if @range.empty?
-    if @languages.nil?
-      @languages = @languages_all
-    else
-      if @languages.present?
-        @languages = @languages.empty? ? @languages_all : params[:language]
-        # @languages = @languages.class == Array ? @languages : [@languages]
-      else
-        @languages = @languages_all
-      end
-    end
-
+    @language = params[:language]
+    @language = Language.first.id if @language.nil? || @language.empty?
     if params[:tags].nil?
       @tags_given = params[:tags].present? ? @tags_all : params[:tags]
     else
@@ -78,11 +70,9 @@ class GroupsController < ApplicationController
 
     if !@location.nil?
       # if more tags than one are given the any should be all in line 28
-      if @location.empty?
-        @groups = Group.where(language_id: @languages).tagged_with(@tags_given, any: true)
-      else
-        @groups = Group.near(@location, @range).where(language_id: @languages).tagged_with(@tags_given, any: true)
-      end
+      @groups = Group.near(@location, @range).where(language_id: @language).tagged_with(@tags_given, any: true)
+    elsif @location.nil?
+      @groups = Group.where(language_id: @language).tagged_with(@tags_given, any: true)
     else
       @groups = Group.all
     end
