b2f6aa013666bb17c10381c114086788bf7e33a9
Anja Laube <mail@anjalaube.com>
2019-12-04 09:43:49 +0100

    Seed file and Form to create group started

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,11 +13,17 @@ class GroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
   end
 
   def create
     @group = Group.new(group_params)
     @group.user_id = current_user.id
+    if @group.save
+      redirect_to groups_path
+    else
+      render "new"
+    end
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -14,3 +14,4 @@ Rails.application.routes.draw do
 
   resources :comments, only: [:create, :destroy]
 end
+
