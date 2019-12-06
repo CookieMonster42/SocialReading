9e053b03eab739a71c3176a27ebd3f1050a575f4
Anja Laube <mail@anjalaube.com>
2019-12-06 11:49:52 +0100

    Meta tags version 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   # def user_not_authorized
   #   flash[:alert] = "You are not authorized to perform this action."
